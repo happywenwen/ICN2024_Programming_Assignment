088b8453a1ed935e108c5a80c798224261034a5d
happywenwen <a0982004811@gmail.com>
2024-03-30 23:19:44 +0800

    edit proxy_server.py: complete cache part

--- p3/proxy_server.py
+++ p3/proxy_server.py
@@ -1,18 +1,22 @@
+import socket
+
 # Set the server IP address and port
 # TODO Start
-HOST, PORT =
+HOST, PORT = "127.0.0.1", 9999
 # TODO end
 
 # Create a server socket, bind it to the specified IP and port, and start listening
 # TODO Start
-serverSocket =
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind((HOST, PORT))
+serverSocket.listen(0)
 # TODO end
 
 while True:
     print('Ready to serve...')
     # Accept an incoming connection and get the client's address
     # TODO Start
-    client_socket, client_address =
+    client_socket, client_address = serverSocket.accept()
     # TODO end
 
     print('Received a connection from:', client_address)
@@ -20,7 +24,7 @@ while True:
     try:
         # Receive request from the client
         # TODO Start
-        request =
+        request = client_socket.recv(1000).decode("utf-8")
         # TODO end
         print(request)
 
@@ -44,6 +48,8 @@ while True:
             client_socket.send("HTTP/1.1 200 OK\r\n".encode('utf-8'))
             client_socket.send("Content-Type:text/html\r\n\r\n".encode('utf-8'))
             # TODO Start
+            for response_body in output_data:
+                client_socket.send(response_body.encode("utf-8"))
             # TODO End
             print('Read from cache')
 
@@ -52,7 +58,7 @@ while True:
             if file_exist == "false":
                 # Create a socket on the proxy server
                 # TODO Start
-                proxy_server_socket =
+                proxy_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 # TODO End
 
                 host_name = filename.replace("www.", "", 1)
@@ -62,6 +68,7 @@ while True:
                     print("Trying to connect to the web server")
                     # Connect the socket to the web server port
                     # TODO Start
+                    proxy_server_socket.connect(("127.0.0.1", 7777))
                     # TODO End
                     print("Connected successfully")
 
@@ -78,24 +85,34 @@ while True:
 
                     # Read the response into buffer
                     # TODO Start
-                    # TODO End
+                    server_message = file_obj.readlines()
+                    html_content = server_message[server_message.index('<!DOCTYPE html>\n'):]
                     print("Read the file from the web server successfully")
 
                     # Create a new file in the cache for the requested file
                     # TODO Start
+                    with open(file_path[1:], "w") as cache_file:
+                        cache_file.writelines(html_content)
                     # TODO End
                     print("Wrote the file to the cache successfully")
 
                     # Send the response to the client socket
                     # TODO Start
+                    client_socket.send("HTTP/1.1 200 OK\r\n".encode('utf-8'))
+                    client_socket.send("Content-Type:text/html\r\n\r\n".encode('utf-8'))
+                    for response_body in html_content:
+                        client_socket.send(response_body.encode("utf-8"))
                     # TODO End
                     print("Sent the data from the web server to the client")
                 except:
                     print("Illegal request")
             else:
-                pass
                 # HTTP response message for file not found
                 # TODO Start
+                response_header = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html; encoding=utf8\r\n\r\n"
+                client_socket.send(response_header.encode("utf-8"))
+                response_body = "<!DOCTYPE html>\n<html>\n<head>\n<title>404 Not Found</title>\n</head>\n<body>\n<h1>404 Not Found</h1>\n</body>\n</html>"
+                client_socket.send(response_body.encode("utf-8"))
                 # TODO End
 
     finally:
