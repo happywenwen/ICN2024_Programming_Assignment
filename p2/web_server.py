c3f117cd9d2679da5ff6548c46fba947a320b281
happywenwen <a0982004811@gmail.com>
2024-03-30 17:19:23 +0800

    edit web_server.py: enable to access html

--- p2/web_server.py
+++ p2/web_server.py
@@ -1,21 +1,52 @@
 import socket
 import sys
 
+# Handle HTTP request
+class HTTPRequest:
+    def __init__(self, request_text):
+        self.method = None
+        self.path = None
+        self.http_version = None
+        self.headers = {}
+        self.body = None
+
+        # Split the request text into lines
+        lines = request_text.split('\r\n')
+        
+        # Parse the request line
+        request_line_parts = lines[0].split()
+        self.method = request_line_parts[0]
+        self.path = request_line_parts[1]
+        self.http_version = request_line_parts[2]
+
+        # Parse headers
+        for line in lines[1:]:
+            if line.strip():
+                key, value = line.split(':', 1)
+                self.headers[key.strip()] = value.strip()
+
+        # Parse body (if present)
+        if '\r\n\r\n' in request_text:
+            body_start_index = request_text.index('\r\n\r\n') + len('\r\n\r\n')
+            self.body = request_text[body_start_index:]
+
 # Server setup
 # Specify the IP address and port number (Use "127.0.0.1" for localhost on local machine)
 # TODO Start
-HOST, PORT =
+HOST, PORT = "127.0.0.1", 7777
 # TODO end
 
 
 # 1. Create a socket
 # 2. Bind the socket to the address
 # TODO Start
-serverSocket =
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind((HOST, PORT))
 # TODO End
 
 # Listen for incoming connections (maximum of 1 connection in the queue)
 # TODO Start
+serverSocket.listen(0)
 # TODO End
 
 # Start an infinite loop to handle incoming client requests
@@ -24,14 +55,14 @@ while True:
 
     # Accept an incoming connection and get the client's address
     # TODO Start
-    connectionSocket, address =
+    connectionSocket, address = serverSocket.accept()
     # TODO End
     print(str(address) + " connected")
 
     try:
         # Receive and decode the client's request
         # TODO Start
-        message =
+        message = connectionSocket.recv(1000).decode("utf-8")
         # TODO End
 
         # If the message is empty, set it to a default value
@@ -43,7 +74,8 @@ while True:
 
         # Extract the filename from the client's request
         # TODO Start
-        filename =
+        request = HTTPRequest(message)
+        filename = request.path.split("/")[1]
         # TODO End
         print(f"Extract the filename: {filename}")
 
@@ -56,6 +88,11 @@ while True:
         # 2. Send the content of the requested file to the client line by line
         # 3. Close the connection to the client
         # TODO Start
+        response_header = "HTTP/1.1 200 OK\r\nContent-Type: text/html; encoding=utf8\r\nConnection: close\r\n\r\n"
+        connectionSocket.send(response_header.encode("utf-8"))
+        for response_body in outputdata:
+            connectionSocket.send(response_body.encode("utf-8"))
+        connectionSocket.close()
         # TODO End
 
     except IOError:
